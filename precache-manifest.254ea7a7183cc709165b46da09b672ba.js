942552fbb9a5f7ff687b9b7c26330f4575a106f0
Cláudio Hilário <claudio.hilario@ontech.pt>
2020-04-13 22:58:43 +0100

    Updates

---
+++ precache-manifest.254ea7a7183cc709165b46da09b672ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17bc6662d025089f514618d962f6ea61",
+    "url": "/@ontech/shortcut-key-sensor/index.html"
+  },
+  {
+    "revision": "1add682ec8ffa467d33d",
+    "url": "/@ontech/shortcut-key-sensor/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "b6fc6bc84f19132ee1a3",
+    "url": "/@ontech/shortcut-key-sensor/static/js/2.1bb9b7a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/@ontech/shortcut-key-sensor/static/js/2.1bb9b7a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1add682ec8ffa467d33d",
+    "url": "/@ontech/shortcut-key-sensor/static/js/main.0d17c37a.chunk.js"
+  },
+  {
+    "revision": "f8f9f6994c6588c25829",
+    "url": "/@ontech/shortcut-key-sensor/static/js/runtime-main.302d1cea.js"
+  }
+]);
